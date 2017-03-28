fafaaa525ebf664e6b659253a8bc1804d3c833dd
Avic <Avicennasis@gmail.com>
2017-03-27 22:36:58 -0400

    Update mikey.sh
    add shebang, using $(..) instead of legacy `..`., double quote to prevent globbing and word splitting

--- mikey.sh
+++ mikey.sh
@@ -1,2 +1,4 @@
-for w in `echo $1 | grep -o .`
-do grep ^$w /usr/share/dict/words | shuf | sed "s/'s$//" | head -n1; done
+#!/bin/bash
+
+for w in $(echo "$1" | grep -o .)
+do grep "^$w" /usr/share/dict/words | shuf | sed "s/'s$//" | head -n1; done
